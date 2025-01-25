f7c3e0f0451c20b62f4a3b5bed364101bb872c00
Factories-git <official.factories@gmail.com>
2025-01-25 13:34:04 +0900

    BruteForcing

--- 회전 초밥.py
+++ 회전 초밥.py
@@ -1,19 +1,17 @@
 n, d, k, c = map(int, input().split())
 sushi_belt = [int(input()) for i in range(n)]
-
-re = k
-if c not in sushi_belt:
-    re += 1
-window = sushi_belt[:k]
-for i in range(k, n+1):
-    window = sushi_belt[i-k:i]
+Check = {i: 0 for i in sushi_belt}
+re = 0
+for i in range(n):
+    window = [sushi_belt[(j + i) % n] for j in range(k)]
     if list(dict.fromkeys(window)) != window:
         continue
-    window.append(sushi_belt[(i-k-1) % n])
+    print(window)
+    window.append(sushi_belt[(i - k - 1) % n])
     if list(dict.fromkeys(window)) == window and window[-1] == c:
         re = max(re, len(window))
     window.pop()
-    window.append(sushi_belt[(i+1) % n])
+    window.append(sushi_belt[(i + 1) % n])
     if list(dict.fromkeys(window)) == window and window[-1] == c:
         re = max(re, len(window))
 print(re)
